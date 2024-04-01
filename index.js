574087ebb81c84e731d712d57ac7b4186b090304
Joel John <johnjoel079@gmail.com>
2024-04-01 16:42:09 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob"; 
+function getCustomerName() {
+    return customerName;
+}
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer(customer) {
+    bestCustomer = "not bob";
+} 
+var bestCustomer; 
+function overwriteBestCustomer(newCustomer) {
+    bestCustomer = newCustomer;
+} 
+ const leastFavoriteCustomer = { value: "Bob" };
+    function changeLeastFavoriteCustomer(newCustomer) {
+        leastFavoriteCustomer = newCustomer;
+
+}
+
